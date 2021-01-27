1be22386f3d0d9b4880a8a9d687c4c548c30aba5
Marcelo Silva <xmarcelo195@gmail.com>
2021-01-27 00:14:40 -0300

    primeira-aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget/'
+import Footer from '../src/components/Footer/'
+import GitHubCorner from '../src/components/GitHubCorner/'
+import QuizBackground from '../src/components/QuizBackground/'
+import QuizLogo from '../src/components/QuizLogo/'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+  <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <Widget>
+        <Widget.Header>
+          <h1>Counter Strike Global Offensive</h1>
+        </Widget.Header>
+        <Widget.Content>
+        <p>lorem ipsum dolor sit amet...</p>
+        </Widget.Content>
+      </Widget>
+
+      <Widget>
+        <Widget.Content>
+          <h1>Quizes da Galera</h1>
+          <p>lorem ipsum dolor sit amet...</p>
+        </Widget.Content>
+      </Widget>
+      <Footer/>
+      <GitHubCorner projectUrl="https://github.com/xmarcelo195/csgoquiz"/>
+    </QuizContainer>
+  </QuizBackground>
+  );
 }
---
+++ src/components/QuizBackground/index.js
@@ -0,0 +1,37 @@
+// src/components/QuizBackground/index.js
+import styled from 'styled-components';
+
+const QuizBackground = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${({ backgroundImage }) => backgroundImage});
+  background-color: ${({ theme }) => theme.colors.colorBg};
+  flex: 1;
+  @media screen and (max-width: 500px) {
+    background-image: none;
+    &:after {
+      content: "";
+      background-size: cover;
+    background-position: center;
+      background-image:
+        linear-gradient(transparent, ${({ theme }) => theme.colors.colorBg}),
+        url(${({ backgroundImage }) => backgroundImage});
+      display: block;
+      width: 100%;
+      /* height: 200px; */
+      height: 400px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 1;
+    }
+    *:first-child {
+      position: relative;
+      z-index: 10;
+    }
+  }
+`;
+
+export default QuizBackground;
